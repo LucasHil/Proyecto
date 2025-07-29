ab9d99380999987895d3de685369cbf9544496d9
Lucas <casluhildebrandt@gmail.com>
2025-07-29 12:18:04 -0300

    API por capas

--- dist/app.js
+++ dist/app.js
@@ -1,64 +1,8 @@
 import express from 'express';
-import { User } from './user.js';
+import { userRouter } from './users/users.routes.js';
 const app = express();
 app.use(express.json());
-const users = [
-    new User('Lucas', 'Hildebrandt', 'casluhildebrandt@gmail.com', 'f15380c7-dde7-4767-a72a-42c520d21808'),
-];
-//funcion zanitizar
-function sanitizeCharacterInput(req, res, next) {
-    req.body.sanitizedInput = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-    };
-    Object.keys(req.body.sanitizedInput).forEach(key => {
-        if (req.body.sanitizedInput[key] === undefined)
-            delete req.body.sanitizedInput[key];
-    });
-    next();
-}
-// ACA EMPIEZA LOS MÉTODOS
-//get all - traemos todos los usuarios
-app.get('/api/users', (req, res) => {
-    res.json(users);
-});
-//get one - traemos un solo usuario
-app.get('/api/users/:id', (req, res) => {
-    const us = users.find((us) => us.id === req.params.id);
-    if (!us) {
-        return res.status(404).send({ message: 'User not found' });
-    }
-    res.json({ data: us });
-});
-//post - crear un nuevo usuario
-app.post('/api/users', sanitizeCharacterInput, (req, res) => {
-    const input = req.body.sanitizedInput;
-    const us = new User(input.firstName, input.lastName, input.email, input.id);
-    users.push(us); //lo agregamos al array
-    return res.status(201).send({ message: 'User created', data: us });
-});
-//put - modificar un usuario
-app.put('/api/users/:id', sanitizeCharacterInput, (req, res) => {
-    const userId = users.findIndex(user => user.id === req.params.id);
-    if (userId === -1) {
-        return res.status(404).send({ message: 'User not found' });
-    }
-    users[userId] = { ...users[userId], ...req.body.sanitizedInput };
-    return res.status(200).send({ message: 'User updated successfully', data: users[userId] });
-});
-//delete -- borrar un usuario
-app.delete('/api/users/:id', (req, res) => {
-    const userId = users.findIndex((user) => user.id === req.params.id);
-    if (userId === -1) {
-        res.status(404).send({ Message: 'User not found' });
-    }
-    else {
-        users.splice(userId, 1);
-        res.status(200).send({ Message: 'User deleted succefully' });
-    }
-});
-//Para cuando un recurso no existe
+app.use('/api/users', userRouter); //Aca podría haber diferentes lista para diferentes recursos? ej. app.use('/api/productos', userRouter), es decir un nuevo CRUD
 app.use((_, res) => {
     return res.status(404).send({ message: 'Resource not found' });
 });
