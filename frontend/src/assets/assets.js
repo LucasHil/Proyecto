2c2860d37e74afe755e4c4771287609404f386ad
Lucas <casluhildebrandt@gmail.com>
2025-09-24 16:30:45 -0300

    Agregando  pages

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,15 @@
+import HL from './HL.svg'
+import HL_png from './HL_png.png'
+import align_right from './align_right.svg'
+import search from './search.svg'
+import shopping_bag from './shopping_bag.svg'
+import user from './user.svg'
+
+export const assets ={
+    HL,
+    HL_png,
+    align_right,
+    search,
+    shopping_bag,
+    user
+}
