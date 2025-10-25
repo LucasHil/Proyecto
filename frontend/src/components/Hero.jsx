0693faf0a6ee6e6cfb8cc735436dba4c9dca35b7
Lucas <casluhildebrandt@gmail.com>
2025-10-25 09:37:53 -0300

    Modify Hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,26 +3,27 @@ import { assets } from '../assets/assets.js'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col sm:flex-row border border-gray-400' >
-        {/*Hero left Side*/}
-        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0' >
-            <div className='text-[#414141]' >
-                <div className='flex items-center gap-2' >
-                    <p className='w-8 md:w-11 h-[2px] bg-[#414141]' ></p>
-                    <p>OUR BESTSELLERS</p>
-                </div>
-                <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed' >Latest Arrivals</h1>
-                <div className='flex items-center gap-2' >
-                    <p className='font-semibold text-sm md:text-base' >SHOP NOW</p>
-                    <p className='w-8 md:w-11 h-[1px] bg-[#414141]' ></p>
-                </div>
-            </div>
-        </div>
-        {/* Hero Right Side */}
-        <img className='w-full sm:w-1/2' src={assets.heroTree} alt="" />
-        
+<div className="relative w-full h-[500px]">
+      {/* Imágenes lado a lado */}
+      <div className="flex flex-col md:flex-row w-full h-full ">
+        <img
+          className="w-1/2 h-full object-cover"
+          src={assets.heroTree}
+          alt="Left"
+        />
+        <img
+          className="w-1/2 h-full object-cover"
+          src={assets.heroTree} 
+          alt="Right"
+        />
+      </div>
+
+      {/* Texto encima de las imágenes */}
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-[#414141] px-4 z-10">
+            <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed">Latest Arrivals</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Hero
